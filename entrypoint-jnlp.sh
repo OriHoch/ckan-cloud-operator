bd9db3753d168284330646e0259002ac93293eaa
Ori Hoch <ori@uumpa.com>
2019-12-28 17:29:13 +0200

    remove CKAN_CLOUD_USER_NAME requiremnt from jnlp Dockerfile (we rely on Jenkins for user permissions)

--- entrypoint-jnlp.sh
+++ entrypoint-jnlp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ -z "${CKAN_CLOUD_USER_NAME}" ] && echo missing CKAN_CLOUD_USER_NAME && exit 1
+# [ -z "${CKAN_CLOUD_USER_NAME}" ] && echo missing CKAN_CLOUD_USER_NAME && exit 1
 ! [ -e /etc/ckan-cloud/.kube-config ] && echo missing /etc/ckan-cloud/.kube-config && exit 1
 ! [ -z "${KUBE_CONTEXT}" ] && kubectl config use-context "${KUBE_CONTEXT}" >/dev/null 2>&1
 export KUBECONFIG=/etc/ckan-cloud/.kube-config
